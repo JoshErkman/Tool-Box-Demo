f039e39824b84644b2ef51699dc69c68dd981f08
Josh Erkman <erkman.josh@gmail.com>
2020-11-19 20:35:09 -0500

    initial commit

---
+++ ToolBoxDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBoxDemo
+{
+    class Program
+    {
+        //acts as our programs .exe file, aka executable
+        //This is the file we double click on to start any program
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to your Tool Box!");
+            Console.WriteLine("You can either hit \"Y\" then ENTER to continue, or hit \"N\" then ENTER to Clear the console and continue.");
+
+            string userResponse = Console.ReadLine();
+            if(userResponse == "Y")
+            {
+                KeyToContinue();
+            }
+            else if(userResponse == "N")
+            {
+                KeyToContinueWithClearConsole();
+            }
+            else
+            {
+                Console.WriteLine("Not sure friend, but I'm gonna close the program when you hit ENTER");
+            }
+           
+            KeyToContinueWithClearConsole();
+            Console.ReadLine();
+
+            //Functions are methods written outside of a custom class
+            //Methods are fuctions written inside of a custom class
+
+            //Functions below this line
+            void KeyToContinue()
+            {
+                Console.WriteLine("Hit ENTER to continue on with the code...");
+                Console.ReadLine();
+
+            }
+            void ClearConsole()
+            {
+                Console.Clear();
+               // Console.WriteLine("Congratulations! You've cleared the console!...hit ENTER to close the program.");
+            }
+            
+            void KeyToContinueWithClearConsole()
+            {
+                KeyToContinue();
+                ClearConsole();
+            }
+
+
+        }
+    }
+}
